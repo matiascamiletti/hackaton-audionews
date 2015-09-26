6a18e59ea8b419ece7754b4d80682645e0a08296
Matias Camiletti <matias.camiletti@gmail.com>
2015-09-26 03:21:17 -0300

    Limpieza de codigo.

--- app/src/main/java/com/mobileia/audionews/fragment/NewsFragment.java
+++ app/src/main/java/com/mobileia/audionews/fragment/NewsFragment.java
@@ -21,27 +21,7 @@ import com.mobileia.audionews.service.ServiceListener;
 
 import java.util.List;
 
-/**
- * A fragment representing a list of Items.
- * <p/>
- * Large screen devices (such as tablets) are supported by replacing the ListView
- * with a GridView.
- * <p/>
- * Activities containing this fragment MUST implement the {@link OnFragmentInteractionListener}
- * interface.
- */
 public class NewsFragment extends Fragment implements AbsListView.OnItemClickListener {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     /**
      * The fragment's ListView/GridView.
@@ -60,13 +40,8 @@ public class NewsFragment extends Fragment implements AbsListView.OnItemClickLis
 
     private boolean isSpeeching = false;
 
-    // TODO: Rename and change types of parameters
-    public static NewsFragment newInstance(String param1, String param2) {
+    public static NewsFragment newInstance() {
         NewsFragment fragment = new NewsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
@@ -81,12 +56,6 @@ public class NewsFragment extends Fragment implements AbsListView.OnItemClickLis
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-        // TODO: Change Adapter to display your content
         mAdapter = new NewsAdapter(getActivity());
         LaNacion.getLastNews(getActivity(), new ServiceListener() {
             @Override
@@ -112,12 +81,6 @@ public class NewsFragment extends Fragment implements AbsListView.OnItemClickLis
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         LNNews n = mAdapter.getList().get(position);
         DetailActivity.createInstance(getActivity(), n);
@@ -134,21 +97,6 @@ public class NewsFragment extends Fragment implements AbsListView.OnItemClickLis
         if (emptyView instanceof TextView) {
             ((TextView) emptyView).setText(emptyText);
         }
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(String id);
     }
 
     public void startSpeech(MCSpeech speech){
